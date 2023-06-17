a2bd75064a76ad1f5bd1127a86f4c3fc2537cd7a
AvinaashAnandK <p17avinaashak@iima.ac.in>
2023-06-18 00:37:53 +0530

    feat: Enhance DataLoader with data type and format checks
    DataLoader now validates the input file format and supports .csv, .txt, and python lists. It requires a 'data_column_name' argument for .csv files to specify which column contains the text corpus. DataLoader now also checks if the column data type is string, and throws an error if not. The data is now loaded into a dataframe only once during DataLoader's initialization.

--- ringgpt/dataloader.py
+++ ringgpt/dataloader.py
@@ -1,6 +1,36 @@
+import pandas as pd
+import os
+
 class DataLoader:
-    def __init__(self, file, chunking_type):
+    def __init__(self, file, chunking_type, data_column_name=None):
         self.file = file
         self.chunking_type = chunking_type
+        self.data_column_name = data_column_name
+        self.dataframe = None
 
-    # Add your methods here
+        self.load_data()
+
+    def load_data(self):
+        # If the data is a Python list
+        if isinstance(self.file, list):
+            self.dataframe = pd.DataFrame(self.file)
+        # If the data is a file path
+        elif isinstance(self.file, str):
+            # Check the file extension
+            _, file_extension = os.path.splitext(self.file)
+            if file_extension not in ['.csv', '.txt']:
+                raise ValueError("Unsupported raw data format, supported formats are .csv, .txt files and python lists.")
+
+            # Load the appropriate file type
+            if file_extension == '.csv':
+                if self.data_column_name is None:
+                    raise ValueError("Data column name is required for .csv files.")
+                self.dataframe = pd.read_csv(self.file, usecols=[self.data_column_name])
+            elif file_extension == '.txt':
+                self.dataframe = pd.read_csv(self.file, sep='\n', header=None)
+
+            # Check the type of the data in the data column
+            if self.dataframe.dtypes[0] != 'object':
+                raise ValueError("Only strings are supported in the data column.")
+        else:
+            raise ValueError("File argument should either be a file path or a python list.")
--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='RingGPT',
     version='0.0.1',
-    url='https://github.com/yourusername/RingGPT',
-    author='Author Name',
-    author_email='author@gmail.com',
+    url='https://github.com/AvinaashAnandK/RingGPT',
+    author='Avinaash Anand K',
+    author_email='avinaash96@gmail.com',
     description='Description of my package',
     packages=find_packages(),    
     install_requires=[],
