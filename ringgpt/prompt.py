36c06831f9bc97dc11d9dd6629b15ddc355901b4
AvinaashAnandK <p17avinaashak@iima.ac.in>
2023-06-18 00:47:03 +0530

    feat: Validate Prompt parameters
    Prompt now validates that the required parameters 'instruction' and 'output_format' are provided. If the optional 'example' parameter is passed, it checks that it's in the expected list of dictionaries format with 'Input' and 'Output' attributes. Appropriate error messages are raised for invalid parameters.

--- ringgpt/prompt.py
+++ ringgpt/prompt.py
@@ -1,7 +1,15 @@
 class Prompt:
     def __init__(self, instruction, output_format, example=None):
+        if not instruction:
+            raise ValueError("Instruction is a mandatory parameter.")
+        if example:
+            if not isinstance(example, list) or not all(isinstance(e, dict) and 'Input' in e and 'Output' in e for e in example):
+                raise ValueError("""Examples is not in expected format. Please pass examples in the format 
+                [{"Input":"Sample corpus 1", "Output":"Expected output 1 in the specified format"},
+                {"Input":"Sample corpus 2", "Output":"Expected output 2 in the specified format"}]""")
+        if not output_format:
+            raise ValueError("Output format is a mandatory parameter.")
+        
         self.instruction = instruction
         self.output_format = output_format
         self.example = example
-
-    # Add your methods here
