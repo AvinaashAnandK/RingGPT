11d23fa40407a4e63d8ec3ae8b6e20f0a4e94858
AvinaashAnandK <p17avinaashak@iima.ac.in>
2023-06-18 12:40:52 +0530

    feat: Validate instruction parameter in Prompt
    Instruction parameter in Prompt class is now validated against a list of valid instructions defined as an enum. An error is thrown if the provided instruction is not recognised.

--- ringgpt/prompt.py
+++ ringgpt/prompt.py
@@ -1,7 +1,24 @@
+from enum import Enum
+
+class Instruction(Enum):
+    UNSTRUCTURED_TO_STRUCTURED = "Unstructured to structured"
+    SUMMARISE = "Summarise"
+    TRANSLATE = "Translate"
+    QA = "QA"
+    SENTIMENT_ANALYSIS = "Sentiment Analysis"
+    SPEECH_RECOGNITION = "Speech Recognition"
+
+
 class Prompt:
-    def __init__(self, instruction, output_format, example=None):
-        if not instruction:
-            raise ValueError("Instruction is a mandatory parameter.")
+    def __init__(self, instruction, output_format, example=None, output_check=None):
+        if not Instruction[instruction]:
+            raise ValueError("""Instruction not recognised. Ring can only take the following instructions:
+            1. Unstructured to structured
+            2. Summarise
+            3. Translate
+            4. QA
+            5. Sentiment Analysis
+            6. Speech Recognition""")
         if example:
             if not isinstance(example, list) or not all(isinstance(e, dict) and 'Input' in e and 'Output' in e for e in example):
                 raise ValueError("""Examples is not in expected format. Please pass examples in the format 
@@ -9,7 +26,12 @@ class Prompt:
                 {"Input":"Sample corpus 2", "Output":"Expected output 2 in the specified format"}]""")
         if not output_format:
             raise ValueError("Output format is a mandatory parameter.")
+        if output_check and not isinstance(output_check, list):
+            raise ValueError("Output check must be a list of strings.")
         
-        self.instruction = instruction
+        self.instruction = Instruction[instruction]
         self.output_format = output_format
         self.example = example
+        self.output_check = output_check
+
+    # Add your methods here
