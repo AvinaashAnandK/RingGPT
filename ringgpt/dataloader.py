cff9b39745425656d7e5b5699a67ecccbd71a36f
AvinaashAnandK <p17avinaashak@iima.ac.in>
2023-06-18 18:49:11 +0530

    Changes to DataLoader class to validate chunking_type

--- ringgpt/dataloader.py
+++ ringgpt/dataloader.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 class DataLoader:
-    def __init__(self, file, chunking_type, data_column_name=None):
+    def __init__(self, file, chunking_type='chunk', data_column_name=None):
         self.file = file
         self.chunking_type = chunking_type
         self.data_column_name = data_column_name
@@ -11,26 +11,31 @@ class DataLoader:
         self.load_data()
 
     def load_data(self):
-        # If the data is a Python list
+        # Validate chunking_type
+        valid_chunking_types = ['truncate', 'chunk']
+        if self.chunking_type not in valid_chunking_types:
+            raise ValueError("Invalid chunking_type. Supported values are 'truncate' and 'chunk'.")
+
+        # Load data from the file or Python list
         if isinstance(self.file, list):
             self.dataframe = pd.DataFrame(self.file)
-        # If the data is a file path
         elif isinstance(self.file, str):
-            # Check the file extension
             _, file_extension = os.path.splitext(self.file)
             if file_extension not in ['.csv', '.txt']:
                 raise ValueError("Unsupported raw data format. Supported formats are .csv, .txt files, and Python lists.")
 
-            # Load the appropriate file type
             if file_extension == '.csv':
+                # Read data from a CSV file
                 if self.data_column_name is None:
                     raise ValueError("Data column name is required for .csv files.")
-                self.dataframe = pd.read_csv(self.file, usecols=[self.data_column_name])
+                self.dataframe = pd.read_csv(self.file)
+                if self.data_column_name is not None and self.dataframe.dtypes[self.data_column_name] != 'object':
+                    raise ValueError("Only strings are supported in the data column.")
             elif file_extension == '.txt':
-                self.dataframe = pd.read_csv(self.file, sep='\n', header=None)
+                # Read data from a text file
+                with open(self.file, 'r') as f:
+                    content = f.read()
+                self.dataframe = pd.DataFrame([content], columns=[self.data_column_name])
 
-            # Check the type of the data in the data column
-            if self.data_column_name is not None and self.dataframe.dtypes[self.data_column_name] != 'object':
-                raise ValueError("Only strings are supported in the data column.")
         else:
             raise ValueError("File argument should either be a file path or a Python list.")
