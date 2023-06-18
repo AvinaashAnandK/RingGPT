33bb485e79f6198035391f1d939d71f8827f446c
AvinaashAnandK <p17avinaashak@iima.ac.in>
2023-06-18 12:11:46 +0530

    feat: Validate Ring initialization and set defaults
    Ring now validates the 'llm_list' parameter and sets default values for 'max_attempts', 'retry_delay', 'throttle_delay', 'log_enabled', and 'wip_save'. An error is thrown if 'llm_list' is not provided, is not in the expected format, or if 'service' or 'access_token' attributes are missing where required. If 'log_enabled' is set to True, a log file 'ring_log.log' is created/updated.

--- ringgpt/ring.py
+++ ringgpt/ring.py
@@ -1,12 +1,34 @@
+import logging
+
 class Ring:
-    def __init__(self, dataloader, prompt, llm_list, max_attempts, retry_delay, throttle_delay, logging, wip_save):
-        self.dataloader = dataloader
-        self.prompt = prompt
+    def __init__(self, llm_list, max_attempts=5, retry_delay=5, throttle_delay=5, log_enabled=True, wip_save=True):
+        # Validate the llm_list
+        if not llm_list:
+            raise ValueError("Failed initialising Ring, llm_list is not declared by the user")
+        if not isinstance(llm_list, list) or not all(isinstance(llm, dict) for llm in llm_list):
+            raise ValueError("""Failed initialising Ring, llm_list does not align with expected format. Please pass llm_list in the format 
+                [{"service":"OpenAIChat", 
+                  "access_token":"enter-your-token-here",
+                  "identifier":"identifier-for-the-account-used-for-the-token"},
+                  {"service":"BardChat", 
+                  "access_token":"enter-your-token-here",
+                  "identifier":"identifier-for-the-account-used-for-the-token"},
+                  {"service":"EdgeChat",}]""")
+        for llm in llm_list:
+            if 'service' not in llm:
+                raise ValueError("Failed initialising Ring, service attribute is not declared by the user in the dictionaries passed in llm_list")
+            if llm['service'] in ["OpenAIChat", "BardChat"] and (not llm.get('access_token')):
+                raise ValueError("Failed initialising Ring, access_token attribute is not declared by the user in the dictionaries passed in llm_list for OpenAIChat or BardChat")
+        
         self.llm_list = llm_list
         self.max_attempts = max_attempts
         self.retry_delay = retry_delay
         self.throttle_delay = throttle_delay
-        self.logging = logging
+        self.log_enabled = log_enabled
         self.wip_save = wip_save
 
-    # Add your methods here
+        # Set up logging
+        if self.log_enabled:
+            logging.basicConfig(filename='ring_log.log', filemode='a', level=logging.INFO, 
+                                format='%(asctime)s - %(levelname)s - %(message)s')
+            logging.info("Ring initialised")
